48e82da3506a2b80c93557c0b4bb3d4acb808863
Ashley Wulber <ashley@system76.com>
2023-05-26 20:15:36 -0400

    fix: use ceil size for positioner

--- sctk/src/application.rs
+++ sctk/src/application.rs
@@ -1705,7 +1705,7 @@ fn run_command<A, E>(
                         let bounds = node.bounds();
                         let (w, h) = ((bounds.width.ceil() + 0.1) as u32, (bounds.height.ceil() + 0.1) as u32);
                         auto_size_surfaces.insert(SurfaceIdWrapper::Popup(popup.id), (w, h, popup.positioner.size_limits, false));
-                        popup.positioner.size = Some((bounds.width as u32, bounds.height as u32));
+                        popup.positioner.size = Some((w, h));
                     }
                     proxy.send_event(Event::Popup(popup::Action::Popup{popup, _phantom}));
                 } else {
