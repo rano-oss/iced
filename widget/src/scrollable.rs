08d0497d2a71139cec40b4613111c7eacc1f31f6
Cory Forsstrom <cforsstrom18@gmail.com>
2023-06-13 08:22:33 -0700

    Make viewport bounds public

--- widget/src/scrollable.rs
+++ widget/src/scrollable.rs
@@ -1260,8 +1260,10 @@ impl Offset {
 pub struct Viewport {
     offset_x: Offset,
     offset_y: Offset,
-    bounds: Rectangle,
-    content_bounds: Rectangle,
+    /// The viewport bounds of the [`Scrollable`].
+    pub bounds: Rectangle,
+    /// The content bounds of the [`Scrollable`].
+    pub content_bounds: Rectangle,
 }
 
 impl Viewport {
